266921baa09b4f2f617febfc943bda31459d43f0
nevi20-meet <nevi20@meet.mit.edu>
2018-07-29 09:28:27 -0400

    Stopping here

---
+++ meetturtle.py
@@ -0,0 +1,7 @@
+Python 3.6.5 (default, Apr  1 2018, 05:46:30) 
+[GCC 7.3.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
