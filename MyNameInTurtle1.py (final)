e51b0696386798d6d2612daf579f68c042c81e4f
nevi20-meet <nevi20@meet.mit.edu>
2018-07-29 11:09:00 -0400

    Stopping here

---
+++ MyNameInTurtle1.py (final)
@@ -0,0 +1,41 @@
+import turtle
+
+# Everything that comes after the # is a 
+# comment.
+# It is a note to the person reading the code.
+# The computer ignores it.
+# Write your code below here...
+
+#Draw the N:
+turtle.penup()
+turtle.goto (-200,-100)
+turtle.pendown()
+turtle.goto (-200,+100)
+turtle.goto (-200+100,-100)
+turtle.goto (-200+100,+100)
+#Draw the E:
+turtle.penup()
+turtle.goto (-200+150,-100)
+turtle.pendown()
+turtle.goto (-200+150,+100)
+turtle.goto (-200+250,+100)
+turtle.goto (-200+150,+100)
+turtle.goto (-200+150,+0)
+turtle.goto (-200+250,+0)
+turtle.goto (-200+150,+0)
+turtle.goto (-200+150,-100)
+turtle.goto (-200+250,-100)
+#Draw the V:
+turtle.penup()
+turtle.goto (-200+300,+100)
+turtle.pendown()
+turtle.goto (-200+350,-100)
+turtle.goto (-200+400,+100)
+#Draw the I
+turtle.penup()
+turtle.goto (-200+450,+100)
+turtle.pendown()
+turtle.goto (-200+450,-100)
+
+# ...and end it before the next line.
+turtle.mainloop()
---
+++ meetturtle1.py
@@ -0,0 +1,82 @@
+Python 3.6.5 (default, Apr  1 2018, 05:46:30) 
+[GCC 7.3.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+Traceback (most recent call last):
+  File "/home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py", line 25, in <module>
+    turtle.pendown(200,-100)
+TypeError: pendown() takes 0 positional arguments but 2 were given
+>>> 
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+>>> 
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+>>> 
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+===== RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py =====
+
+= RESTART: /home/student/nevi20_lab1/meet2018y1lab1/MEETinTurtle.py (final) =
+>>> 
